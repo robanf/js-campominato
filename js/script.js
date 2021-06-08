3a09b7af046ddabe96bcacfd52168f5163c42531
Roberto <robertoanfuso98@gmail.com>
2021-06-08 14:59:24 +0200

    devo risolvere un piccolo bug nel do while

---
+++ js/script.js
@@ -0,0 +1,46 @@
+var a=[];
+var i=0;
+while(i<16){
+    var temp=Math.floor(Math.random()*100+1);
+    find=nonripetere(a,temp);
+    if(find==true){
+        a.push(temp);
+        i++;
+    }
+}
+console.log(a);
+
+var b=[];
+i=0;
+var cont=0;
+var boom=false;
+while(i<(100-16) && boom==false){
+    var temp= prompt("inserisci un nuovo numero")
+    do{
+        find=nonripetere(b,temp);
+        if(find==false){
+            temp=prompt("devi inserire un numero nuovo")
+        }
+    }while(find==false && temp>100 && temp<1)
+    
+    find=nonripetere(a,temp);
+    if(find==true){
+        b.push(temp);
+        cont++;
+    }else{
+        boom=true;
+    }
+}
+console.log(b);
+console.log(cont);
+
+
+
+function nonripetere(x,y){
+    for(var h=0;h<x.length;h++){
+        if(x[h]==y){
+            return false;
+        }
+    }
+    return true;
+}
